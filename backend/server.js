7bbc15513587c95dd2f078fb6f49c4a65a507f80
Mohammad Mehradnia <mehradnia.m97@gmail.com>
2025-02-16 14:19:02 +0100

    Execute backend on port 80

--- backend/server.js
+++ backend/server.js
@@ -3130,7 +3130,7 @@ mongoose.connect(process.env.MONGO_URI
   .then(() => {
     console.log("Connected to MongoDB Atlas");
     // Start the server after successful database connection
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 80;
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
